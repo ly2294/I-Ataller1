dea874f50a120f387234ce2dfdb9c85663c5d72a
leidy yuliana quintero <lyquintero94@gmail.com>
2018-02-11 19:00:15 -0500

    taller 1 Inteligencia Artificial

---
+++ Algaritmos-.py
@@ -0,0 +1,34 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import statistics as stats
+edades = [22, 23, 26, 26, 26, 34, 34, 38, 40, 41]
+print(stats.median(edades))
+
+
+# In[2]:
+
+
+import statistics as stats
+edades = [22, 23, 26, 26, 26, 34, 34, 38, 40, 41]
+print(stats.mode(edades))
+
+
+# In[3]:
+
+
+import statistics as stats
+muestra_edades = [22, 23, 26, 26, 34, 38, 40]
+print(stats.variance(muestra_edades))
+
+
+# In[ ]:
+
+
+import statistics as stats
+muestra_edades = [22, 23, 26, 26, 34, 38, 40]
+print(stats.stdev(muestra_edades))
+
